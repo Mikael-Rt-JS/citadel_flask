bd9dcf1260bfab958de2e378be18dcfba3824bf7
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2024-08-01 02:26:51 +0400

    Update main.py

--- doc/main.py
+++ doc/main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,make_response,session,url_for
 
 main=Flask(__name__)
-
+main.config['SECRET_KEY']='3e807657127214ede4618d938d613d64083ea834'
 
 @main.route('/')
 @main.route('/home')
