ff4f0d4f96cb8ab1ab771c070708a2bfad0eb839
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2024-08-01 10:32:42 +0400

    Update main.py

--- doc/main.py
+++ doc/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,make_response,session,url_for
+from flask import Flask,render_template,request,make_response,session,redirect
 
 main=Flask(__name__)
 main.config['SECRET_KEY']='3e807657127214ede4618d938d613d64083ea834'
